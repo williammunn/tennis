368b0b816c76b0efdb58b29daf8f6451eb914140
williammunn <95544651+williammunn@users.noreply.github.com>
2021-12-27 21:22:15 +0000

    Update tennis.R

--- tennis.R
+++ tennis.R
@@ -1,11 +1,8 @@
 # tennis code
 rm(list=ls())
 
-require(dplyr,lubridate,ggplot2)
-library(dplyr,lubridate,ggplot2)
-
 # define play.match
-play.match <- function(arg.best.of,arg.match.first.server,arg.match.p1.serve.pct,arg.match.p2.serve.pct) {
+play.match <- function(arg.best.of,arg.match.first.server,arg.match.p1.serve.pct,arg.match.p2.serve.pct,arg.match.print.score=FALSE) {
   # set initial values
   match.over <- FALSE
   current.server <- arg.match.first.server
@@ -13,9 +10,17 @@ play.match <- function(arg.best.of,arg.match.first.server,arg.match.p1.serve.pct
   p1.sets <- 0
   p2.sets <- 0
   set.winner <- ''
+  sets.played <- 0
+  set.winners <- character()
   # play sets until the match is over
   while(match.over == FALSE) {
-    set.winner <- play.set(arg.set.first.server = current.server, arg.set.p1.serve.pct = arg.match.p1.serve.pct, arg.set.p2.serve.pct = arg.match.p2.serve.pct)
+    set.winner <- play.set(arg.set.first.server = current.server, arg.set.p1.serve.pct = arg.match.p1.serve.pct, arg.set.p2.serve.pct = arg.match.p2.serve.pct,arg.set.print.score = arg.match.print.score)
+    # swap server
+    current.server <- ifelse(current.server == 'P1','P2','P1') # this bit of code is not working, because it needs to be able to look at who was last serving, defined within play.set
+    # increment number of sets played
+    sets.played <- sets.played + 1
+    # record the set winner
+    set.winners[sets.played] <- set.winner
     # increment the set winner's count
     if (set.winner == 'P1') {
       p1.sets <- p1.sets + 1
@@ -33,11 +38,12 @@ play.match <- function(arg.best.of,arg.match.first.server,arg.match.p1.serve.pct
   } else {
     match.winner <- 'P2'
   }
+  if(arg.match.print.score){print(paste0(match.winner,' won the match ',p1.sets,'-',p2.sets))}
   return(match.winner)
 }
 
 # define play.set
-play.set <- function(arg.set.first.server,arg.set.p1.serve.pct,arg.set.p2.serve.pct) {
+play.set <- function(arg.set.first.server,arg.set.p1.serve.pct,arg.set.p2.serve.pct,arg.set.print.score=FALSE) {
   # initial values
   set.over <- FALSE
   p1.games <- 0
@@ -48,12 +54,16 @@ play.set <- function(arg.set.first.server,arg.set.p1.serve.pct,arg.set.p2.serve.
   # play games until the set is over
   while(set.over == FALSE) {
     # play a game
-    game.winner <- play.game(arg.game.server = current.server, arg.game.serve.pct = ifelse(current.server == 'P1', arg.set.p1.serve.pct, arg.set.p2.serve.pct))
+    game.winner <- play.game(arg.game.server = current.server, arg.game.serve.pct = ifelse(current.server == 'P1', arg.set.p1.serve.pct, arg.set.p2.serve.pct), arg.game.print.score = arg.set.print.score)
     # award winner a game
     if (game.winner == 'P1') {
       p1.games <- p1.games + 1
     } else {
       p2.games <- p2.games + 1
+    }
+    # check if set is over
+    if((p1.games == 6 & p2.games <= 4) | (p2.games == 6 & p1.games <= 4) | p1.games == 7 | p2.games == 7) {
+      set.over <- TRUE
     }
     # swap server
     current.server = ifelse(current.server == 'P1','P2','P1')
@@ -62,11 +72,14 @@ play.set <- function(arg.set.first.server,arg.set.p1.serve.pct,arg.set.p2.serve.
       tiebreak <- TRUE
     }
     if(tiebreak==TRUE) {
-      game.winner <- play.tiebreak(arg.tiebreak.first.server = current.server, arg.tiebreak.p1.serve.pct = arg.set.p1.serve.pct, arg.tiebreak.p2.serve.pct = arg.set.p2.serve.pct)
-    }
-    # check if set is over
-    if((p1.games == 6 & p2.games <= 4) | (p2.games == 6 & p1.games <= 4) | p1.games == 7 | p2.games == 7) {
+      game.winner <- play.tiebreak(arg.tiebreak.first.server = current.server, arg.tiebreak.p1.serve.pct = arg.set.p1.serve.pct, arg.tiebreak.p2.serve.pct = arg.set.p2.serve.pct, arg.tiebreak.print.score = arg.set.print.score)
       set.over <- TRUE
+      # award winner of tiebreak a game
+      if (game.winner == 'P1') {
+        p1.games <- p1.games + 1
+      } else {
+        p2.games <- p2.games + 1
+      }
     }
   }
   if (p1.games > p2.games) {
@@ -74,11 +87,13 @@ play.set <- function(arg.set.first.server,arg.set.p1.serve.pct,arg.set.p2.serve.
   } else {
     set.winner <- 'P2'
   }
+  # print outcome of set
+  if(arg.set.print.score){print(paste0(set.winner,' won the set, ',p1.games,'-',p2.games))}
   return(set.winner)
 }
 
 # define play.game
-play.game <- function(arg.game.server, arg.game.serve.pct) {
+play.game <- function(arg.game.server, arg.game.serve.pct, arg.game.print.score=FALSE) {
   # initial values
   p1.points <- 0
   p2.points <- 0
@@ -93,7 +108,7 @@ play.game <- function(arg.game.server, arg.game.serve.pct) {
     } else {
       p2.points <- p2.points + 1
     }
-    if (p1.points == 5 & p2.points == 5) {
+    if (p1.points == 4 & p2.points == 4) {
       p1.points <- 3
       p2.points <- 3
     }
@@ -108,6 +123,7 @@ play.game <- function(arg.game.server, arg.game.serve.pct) {
   } else {
     game.winner <- 'P2'
   }
+  if(arg.game.print.score){print(paste0(game.winner,' won the game, ',p1.points,'-',p2.points,' ',arg.game.server,' was serving'))}
   return(game.winner)
 }
 
@@ -131,7 +147,7 @@ play.point <- function(arg.point.server, arg.point.serve.pct) {
   }
 }
 
-play.tiebreak <- function(arg.tiebreak.first.server,arg.tiebreak.p1.serve.pct,arg.tiebreak.p2.serve.pct) {
+play.tiebreak <- function(arg.tiebreak.first.server,arg.tiebreak.p1.serve.pct,arg.tiebreak.p2.serve.pct,arg.tiebreak.print.score=FALSE) {
   # initial values
   tiebreak.over <- FALSE
   tiebreak.current.server <- arg.tiebreak.first.server
@@ -183,9 +199,10 @@ play.tiebreak <- function(arg.tiebreak.first.server,arg.tiebreak.p1.serve.pct,ar
     tiebreak.winner <- 'P2'
   }
   # return the winner of the tiebreak
+  if(arg.tiebreak.print.score){print(paste0(tiebreak.winner,' won the tiebreak ',p1.points,'-',p2.points))}
   return(tiebreak.winner)
 }
 
-play.match(arg.best.of = 3, arg.match.first.server = 'P1', arg.match.p1.serve.pct = 0.6, arg.match.p2.serve.pct = 0.8)
+play.match(arg.best.of = 5, arg.match.first.server = 'P1', arg.match.p1.serve.pct = 0.9, arg.match.p2.serve.pct = 0.9, arg.match.print.score = TRUE)
 
 
