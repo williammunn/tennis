15b5edda2b29d189f33406a57587cd8365170bff
williammunn <95544651+williammunn@users.noreply.github.com>
2022-02-09 21:46:06 +0000

    Add files via upload

--- simulate_tournament.R
+++ simulate_tournament.R
@@ -12,16 +12,10 @@ players <- unique(c(matches[,winner_id],matches[,loser_id]))
 
 # matrix to store elo ratings (one row per player)
 matrix.elo <- matrix(data = 1500, nrow = length(players), ncol = 1, dimnames = list(players,"elo"))
-matrix.results <- matrix(data = 0, nrow = 128, ncol = 7, dimnames = list(players, c("R128","R64","R32","R16","QF","SF","F")))
-matrix.matchid <- matrix.results
-for (round in 1:ncol(matrix.matchid)) {
-  matrix.matchid[,round] <- sort(rep(c(1:(nrow(matrix.matchid)/2^round)),2^round))
-}
-rm(round)
 
 # winner of a match
-match.winner <- function(arg.p1,arg.p2) {
-  if(runif(1) < (1/(1 + 10^((matrix.elo[arg.p1,1] - matrix.elo[arg.p2,1])/400)))) {
+match.winner <- function(arg.p1elo,arg.p2elo) {
+  if(runif(1) < (1/(1 + 10^((arg.p2elo - arg.p1elo)/400)))) {
     return(arg.p1)
   } else {
     return(arg.p2)
@@ -29,7 +23,17 @@ match.winner <- function(arg.p1,arg.p2) {
 }
 
 # play the tournament
-play.tournament <- function() {
+play.tournament <- function(
+  arg.playerlist, # a vector with a list of player id's arranged like the draw of the first round
+  arg.elo # a data table having a player_id and an elo column
+  ) {
+  # create matrix objects to store results
+  matrix.results <- matrix(data = 0, nrow = 128, ncol = 7, dimnames = list(players, c("R128","R64","R32","R16","QF","SF","F")))
+  matrix.matchid <- matrix.results
+  for (round in 1:ncol(matrix.matchid)) {
+    matrix.matchid[,round] <- sort(rep(c(1:(nrow(matrix.matchid)/2^round)),2^round))
+  }
+  # main logic
   for (round in c(1:ncol(matrix.matchid))) {
     for (matchnum in c(1:max(matrix.matchid[,round]))) {
       if (round == 1) {
@@ -42,7 +46,7 @@ play.tournament <- function() {
         p2 <- players[(matrix.matchid[, round] == matchnum) & (matrix.results[, round-1] == 1)][2]
       }
       # play match using elo win probabilities
-      winner <- match.winner(p1, p2)
+      winner <- match.winner(arg.elo[player_id==p1,elo], arg.elo[player_id==p2,elo])
       loser <- ifelse(winner == p1, p2, p1)
       # record win in results matrix
       matrix.results[winner, round] <- 1
@@ -52,5 +56,4 @@ play.tournament <- function() {
   return(list(winner,matrix.results))
 }
 
-winner <- play.tournament()[[1]]
-results <- play.tournament()[[2]]
+winner <- play.tournament(players,matrix.elo)[[1]]
