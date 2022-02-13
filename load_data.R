f5e648c46303eed16ba6d92ffe37941811ddb6d1
williammunn <95544651+williammunn@users.noreply.github.com>
2022-02-13 13:55:12 +0000

    Add files via upload
    Ensure that all datasets are data.tables by default

--- load_data.R
+++ load_data.R
@@ -6,12 +6,11 @@ setwd('/Users/vickimunn/Desktop/R Stuff/github/Data/')
 
 # load match data
 files <- list.files(pattern="atp_matches_[^_]*.csv")
-Data <- do.call("rbind", lapply(files, function(x) read.csv(x, sep=",",stringsAsFactors = FALSE)))
+Data <- do.call("rbind", lapply(files, function(x) fread(x)))
 
 # clean data
-Data$tourney_date <- as.Date(as.character(Data$tourney_date),format='%Y%m%d', origin = "1900/01/01")
-Data <- Data %>% mutate(winner_id = as.character(winner_id),
-                        loser_id = as.character(loser_id))
+Data[,tourney_date := as.Date(as.character(tourney_date) ,format='%Y%m%d', origin = "1900/01/01")]
+Data[,`:=`(winner_id = as.character(winner_id),loser_is = as.character(loser_id))]
 
 # dictate what variables get assigned to what data
 match.vars <- c(
@@ -47,33 +46,28 @@ player.vars <- c(
 )
 
 # prepare the match data, which is simply a subet (source Data is already one row per match)
-match.data <- Data[,match.vars]
+match.data <- Data[,.SD,.SDcols = match.vars]
 
 # prepare tournament data
-tourney.data <- Data[,tourney.vars] %>%
-  distinct()
+tourney.data <- Data[,.SD,.SDcols = tourney.vars] %>% distinct()
 
 # prepare seedings data
-seedings.data <- Data[,seed.vars]
+seedings.data <- Data[,.SD,.SDcols = seed.vars]
 seedings.data2 <- rbind(
-  (seedings.data[!is.na(seedings.data$winner_seed),c('tourney_id','winner_id','winner_seed')] %>% rename(player_id = winner_id, seed = winner_seed)),
-  (seedings.data[!is.na(seedings.data$loser_seed),c('tourney_id','loser_id','loser_seed')] %>% rename(player_id = loser_id, seed = loser_seed))
-) %>% distinct() %>% 
-  arrange(tourney_id)
+  seedings.data[!is.na(winner_seed),.(tourney_id,winner_id,winner_seed)][,.(tourney_id, player_id = winner_id, seed = winner_seed)],
+  seedings.data[!is.na(loser_seed),.(tourney_id,loser_id,loser_seed)][,.(tourney_id, player_id = loser_id, seed = loser_seed)])[order(tourney_id)] %>% distinct()
 seedings.data <- seedings.data2
 rm(seedings.data2,files,match.vars,seed.vars,tourney.vars)
 
 # prepare player data
-player.data <- Data[,player.vars]
+player.data <- Data[,.SD,.SDcols = player.vars]
 player.data2 <- rbind(
-  player.data[,c('winner_id','winner_name')] %>% rename(player_id = winner_id, player_name = winner_name),
-  player.data[,c('loser_id','loser_name')] %>% rename(player_id = loser_id, player_name = loser_name) 
-) %>% distinct()
-player.data <- player.data2 %>% arrange(player_id)
+  player.data[,.(winner_id,winner_name)][,.(player_id = winner_id, player_name = winner_name)],
+  player.data[,.(loser_id,loser_name)][,.(player_id = loser_id, player_name = loser_name)])[order(player_id)] %>% distinct()
+player.data <- player.data2
 rm(player.data2,player.vars)
 
 # ensure no duplicates in the player data
-qa.1 <- player.data %>% group_by(player_id) %>% summarise(rows = n()) %>% filter(rows > 1)
-qa.2 <- player.data %>% group_by(player_name) %>% summarise(rows = n()) %>% filter(rows > 1)
-rm(qa.1,qa.2)
+qa1 <- player.data[,.(count = .N),by = player_id][count>1,]
+rm(qa1)
 
