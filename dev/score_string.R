2b6d7fd27f6355e2381062f822fb955f63cc3ca2
williammunn <95544651+williammunn@users.noreply.github.com>
2022-02-20 13:21:24 +0000

    Add files via upload

---
+++ dev/score_string.R
@@ -0,0 +1,109 @@
+rm(list=ls())
+
+# convert a string representing the score into numeric variables
+#convert.score <- function(arg.score.string) {
+  
+  arg.score.string <- "7-6(5) 6-7(8) 6-1 4-2 40-15"
+  completed <- FALSE
+  
+  # initial values
+  p1.total.games <- 0
+  p2.total.games <- 0
+  p1.total.tiebreaks <- 0
+  p2.total.tiebreaks <- 0
+  
+  score.indexes <- c(0,sapply(gregexpr(pattern = " ", arg.score.string)[[1]], FUN = function(x) {x+1}))
+  scores <- character(length(score.indexes)-1)
+  for (i in 1:length(score.indexes)) {
+    scores[i] <- substr(arg.score.string,score.indexes[i],ifelse(is.na(score.indexes[i+1]-2),nchar(arg.score.string),score.indexes[i+1]-2))
+  } ; rm(i)
+  
+  # if the match is not completed, the last element of 'scores' won't a set
+  if (!completed) {
+    num.sets <- length(score.indexes)-1
+    num.completed.sets <- num.sets-1
+  } else{
+    num.sets <- length(score.indexes)
+    num.completed.sets <- num.sets
+  }
+  
+  # function to get the winner of each completed set
+  get.set.winner <- function(arg.set.string) {
+    p1.games <- as.integer(substr(arg.set.string,1,1))
+    p2.games <- as.integer(substr(arg.set.string,3,3))
+    if ((p1.games == 6 & p2.games < 5) | (p1.games == 7)) {
+      return(list("P1",p1.games,p2.games))
+    } else if ((p2.games == 6 & p1.games < 5) | (p2.games == 7)) {
+      return(list("P2",p1.games,p2.games))
+    } else {
+      return("error")
+    }
+  }
+  
+  # retrieve winners of completed sets
+  set.winners <- character(num.completed.sets)
+  if (num.completed.sets > 0) {
+    for (set.num in 1:num.completed.sets) {
+      set.winners[set.num] <- get.set.winner(scores[set.num])[[1]]
+      p1.total.games <- p1.total.games + min(6,get.set.winner(scores[set.num])[[2]])
+      p2.total.games <- p2.total.games + min(6,get.set.winner(scores[set.num])[[3]])
+      p1.total.tiebreaks <- p1.total.tiebreaks + ifelse(get.set.winner(scores[set.num])[[1]]==7,1,0)
+      p2.total.tiebreaks <- p2.total.tiebreaks + ifelse(get.set.winner(scores[set.num])[[2]]==7,1,0)
+    }
+  }
+  
+  # extract number of completed sets won
+  p1.sets <- sum(set.winners == "P1")
+  p2.sets <- sum(set.winners == "P2")
+  
+  # get the number of games in the current set
+  if (num.completed.sets > 0) {
+    p1.games <- as.integer(substr(arg.score.string,object.indexes[num.sets-1],object.indexes[num.sets-1]))
+    p2.games <- as.integer(substr(arg.score.string,object.indexes[num.sets-1]+2,object.indexes[num.sets-1]+2))
+  } else {
+    p1.games <- as.integer(substr(arg.score.string,1,1))
+    p2.games <- as.integer(substr(arg.score.string,3,3))
+  }
+  
+  # determine if a tiebreak is currently happening
+  if(p1.games == 6 & p2.games == 6) {
+    tiebreak.ind <- TRUE
+  } else {
+    tiebreak.ind <- FALSE
+  }
+  
+  # index of "-" for points
+  dash.index <- tail(gregexpr(pattern = "-", arg.score.string)[[1]],n=1)
+  
+  # get the number of points in the current game, depends
+  p1.points <- substr(arg.score.string,object.indexes[num.sets],dash.index-1)
+  p2.points <- substr(arg.score.string,dash.index+1,nchar(arg.score.string))
+  
+  # if not a tiebreak then need to convert scores like 40 and 30 to integer number of points
+  if(!(tiebreak.ind)) {
+    p1.points <- switch(p1.points,
+                        "0" = 0,
+                        "15" = 1,
+                        "30" = 2,
+                        "40" = 3,
+                        "Ad" = 4)
+    p2.points <- switch(p2.points,
+                        "0" = 0,
+                        "15" = 1,
+                        "30" = 2,
+                        "40" = 3,
+                        "Ad" = 4)
+  }
+  p1.points <- as.integer(p1.points)
+  p2.points <- as.integer(p2.points)
+  
+  #return(list("p1.sets" = p1.sets,
+  #            "p2.sets" = p2.sets,
+  #            "p1.games" = p1.games,
+  #            "p2.games" = p2.games,
+  #            "p1.points" = p1.points,
+  #            "p2.points" = p2.points,
+  #            "tiebreak.ind" = tiebreak.ind))
+  
+#}
+
