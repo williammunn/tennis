42d73325d68f0e03ce0d8e2641932416c404500d
williammunn <munnwill@gmail.com>
2022-03-20 18:38:32 +0000

    developed article

--- rmarkdown/001_simulating_matches/001_simulating_matches.R
+++ rmarkdown/001_simulating_matches/001_simulating_matches.R
@@ -8,7 +8,7 @@ source("load_data.R")
 
 # data for actual matches, convert to player-centric
 rm(player.data,seedings.data,tourney.data,dir)
-stats <- Data[,.(tourney_name,tourney_date,winner_name,loser_name,score,w_SvGms,w_bpSaved,w_bpFaced,l_SvGms,l_bpSaved,l_bpFaced,w_svpt,w_1stWon,w_2ndWon,l_svpt,l_1stWon,l_2ndWon)
+stats <- Data[,.(tourney_name,tourney_date,best_of,winner_name,loser_name,score,w_SvGms,w_bpSaved,w_bpFaced,l_SvGms,l_bpSaved,l_bpFaced,w_svpt,w_1stWon,w_2ndWon,l_svpt,l_1stWon,l_2ndWon)
               ][,`:=`(
                   w_sv = (w_1stWon + w_2ndWon)/w_svpt,
                   l_sv = (l_1stWon + l_2ndWon)/l_svpt)
@@ -18,7 +18,7 @@ stats <- Data[,.(tourney_name,tourney_date,winner_name,loser_name,score,w_SvGms,
                   ][,`:=`(
                     w_diff = w_sv - w_rt,
                     l_diff = l_sv - l_rt)
-                    ][,.(tourney_name,tourney_date,winner_name,loser_name,score,w_SvGms,w_bpSaved,w_bpFaced,l_SvGms,l_bpSaved,l_bpFaced,w_sv,w_rt,w_diff,l_sv,l_rt,l_diff)]
+                    ][,.(tourney_name,tourney_date,best_of,winner_name,loser_name,score,w_SvGms,w_bpSaved,w_bpFaced,l_SvGms,l_bpSaved,l_bpFaced,w_sv,w_rt,w_diff,l_sv,l_rt,l_diff)]
 
 # remove matches that ended in retirement or were walkovers, and matches without stats
 stats <- stats[!(substr(score,nchar(score)-2,nchar(score))  %in% c('RET','W/O')),]
@@ -92,16 +92,20 @@ deuces <- sapply(1:1000, function(x) {
 mean(deuces)
 # 7.6
 
-# john isner's % of points won on serve in 2021
+# john isner's % of points won on serve in 2019
 stats[(winner_name == 'John Isner' | loser_name == 'John Isner'),
 ][,sv := ifelse(winner_name == 'John Isner',w_sv,l_sv)][,.(mean(sv),.N), by = year(tourney_date)]
+
+# john isner's % of points won on return in 2019
+stats[(winner_name == 'John Isner' | loser_name == 'John Isner'),
+][,sv := ifelse(winner_name == 'John Isner',w_rt,l_rt)][,.(mean(sv),.N), by = year(tourney_date)]
 
 # probability of winning a game given different % service points won
 winners <- sapply(1:1000,function(x) {play.game("P1",0.75)[[1]]})
 sum(winners=="P1")/length(winners)
 
 # Isner's actual % of sv games won
-stats[(winner_name == 'John Isner' | loser_name == 'John Isner') & year(tourney_date) == 2022,
+stats[(winner_name == 'John Isner' | loser_name == 'John Isner') & year(tourney_date) == 2019,
 ][,`:=`(
   w_svgames_won = w_SvGms - w_bpFaced + w_bpSaved,
   l_svgames_won = l_SvGms - l_bpFaced + l_bpSaved)
@@ -109,16 +113,36 @@ stats[(winner_name == 'John Isner' | loser_name == 'John Isner') & year(tourney_
   svgames_won = ifelse(winner_name == 'John Isner',w_svgames_won,l_svgames_won),
   svgames = ifelse(winner_name == 'John Isner', w_SvGms, l_SvGms))
   ][,lapply(.SD,sum),.SDcols=c("svgames","svgames_won")]
+586/623
 
 # simulated probability of holding serve
 serve_pct <- rep(c(0.1,0.25,0.5,0.75,0.9),1000)
 outcomes <- sapply(serve_pct,function(x) play.game("P1",x)[[1]])
 data.table(points_pct = serve_pct, winner = outcomes)[,.(games_pct = sum(winner=="P1")/.N),by=points_pct]
 
-# 65% of points won
-serve_pct <- rep(0.7,1000)
+# 70% of points won
+serve_pct <- rep(0.65,1000)
 outcomes <- sapply(serve_pct,function(x) play.game("P1",x)[[1]])
 data.table(points_pct = serve_pct, winner = outcomes)[,.(games_pct = sum(winner=="P1")/.N),by=points_pct]
+
+# theoretical % of matches won with 75% / 30% attributes
+winners <- sapply(1:2000,function(x) {play.match(3,"P1",0.75,0.70)[[1]]})
+sum(winners=="P1")/length(winners)
+# 70.55%
+
+# adjust strategy
+winners <- sapply(1:2000,function(x) {play.match(3,"P1",0.70,0.65)[[1]]})
+sum(winners=="P1")/length(winners)
+# 71.75%
+
+# Isner match win % in 2019
+stats[winner_name == 'John Isner' | loser_name == 'John Isner'][, won := ifelse(winner_name == 'John Isner',1,0)
+][year(tourney_date) == 2019,.(.N,pct = sum(won)/.N), by = .(best_of)]
+
+
+
+
+
 
 # record how the result changes with respect to number of simulatons
 #ntimes <- 2000
